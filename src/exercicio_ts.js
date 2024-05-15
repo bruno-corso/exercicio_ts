1108c9e796ce5bfaf3519891b17389aec2f494f7
bruno-corso <brunotcorso@gmail.com>
2024-05-15 19:16:02 -0300

    first commit

---
+++ src/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+const multiplica = (a, b) => {
+    return (a * b);
+};
+const receberNome = (nome) => {
+    return ("Olá " + nome);
+};
+console.log(multiplica(3, 9));
+console.log(receberNome("Bruno"));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,11 @@
+
+const multiplica = (a: number, b: number): number => {
+    return (a*b);
+}
+
+const receberNome = (nome: string): string => {
+    return ("Olá "+ nome)
+}
+
+console.log(multiplica(3,9));
+console.log(receberNome("Bruno"));
